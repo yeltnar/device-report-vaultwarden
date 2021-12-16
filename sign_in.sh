d2b5eded6e37bfccc9e28bd294e91ad562ba1fe7
Andrew Brantley <github@andbrant.com>
2021-12-16 00:38:01 -0600

    better signin?

--- sign_in.sh
+++ sign_in.sh
@@ -3,7 +3,7 @@
 passwordfile="gitignore/passwordfile"
 outfile="./gitignore/BW_SESSION"
 
-bw login $email --passwordfile $passwordfile --raw > "$outfile" 
+bw login $email --passwordfile "$passwordfile" --raw > "$outfile" 
 
 printf "$outfile\n"
 
